2d833227981d01ef8c3bfdd41cc1311caba82e51
athirakcdev <athirakc20@gmail.com>
2023-11-18 18:19:11 +0530

    created a model class for expense

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:expense_tracker/expense.dart';
+import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Expense(),
+    home: Expenses(),
   ));
 }
